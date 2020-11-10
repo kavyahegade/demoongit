682a55241f1f954c23a0b4c1e02fd4d7ee29d9b3
kavyahegade <kavyashreeubdt@gmail.com>
2020-11-10 17:14:09 +0530

    first commit

---
+++ Gitdemo/src/main/java/com/kh/GitdemoApplication.java
@@ -0,0 +1,22 @@
+package com.kh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GitdemoApplication {
+	
+	@GetMapping("/eve")
+	public String saybye() {
+		return "everyone bye";
+		
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitdemoApplication.class, args);
+	}
+
+}
